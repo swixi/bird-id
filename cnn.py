693e8d36e2e2d978530248c5b8d6b9f3e489c7bb
brian <swixee@gmail.com>
2019-06-24 20:39:55 -0400

    initial commit

---
+++ cnn.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+
+import numpy as np
+
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from keras import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+
+from sklearn.model_selection import train_test_split
+
+from PIL import Image
+
+import matplotlib.pyplot as plt
+from matplotlib import image
+
+#CROP_X =
+#960x320+62+0
+
+image_path = os.getcwd() + '/data/images'
+images = []
+labels = []
+
+print(os.listdir(image_path))
+
+for img_file in os.listdir(image_path):
+    img = Image.open(os.path.join(image_path, img_file)).convert('L')
+    #img = img.crop((960,320,62,0))
+    img_array = np.asarray(img)
+
+    #plt.imshow(img_array, cmap='gray', vmin=0, vmax=255)
+    #plt.show()
+
+    images.append(img_array)
+    # Parse the species as a label
+    labels.append(img_file.split(".")[0].split("-")[1])
+
+for img in images:
+    print(img)
+
+X_train, X_val, y_train, y_val = train_test_split(images, labels, test_size=0.1, random_state=97)
+
+
+
+
+one_hot_train = to_categorical(y_train)
+one_hot_val = to_categorical(y_val)
+# one_hot_testing = to_categorical(test_y)
+
+X_train_shaped = X_train.reshape(9, X_train[0].shape[0], X_train[0].shape[1], 1)
+X_val_shaped = X_val.reshape(1, X_val[0].shape[0], X_val[0].shape[1], 1)
+# test_X_shaped = test_X.reshape(10000,28,28,1)
+
+quit()  
+
+
+model = Sequential()
+
+model.add(Conv2D(filters=32, input_shape=(28,28,1), data_format="channels_last", kernel_size=2, strides=2, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Flatten())
+model.add(Dense(1, activation='softmax'))
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.summary()
+
+model.fit(X_train_shaped, one_hot_train, validation_data=(X_val_shaped, one_hot_val), epochs=10)
+
+#model.predict(test_X_shaped[:4])
---
+++ downloader.py
@@ -0,0 +1,66 @@
+import json
+import os
+import argparse
+
+import requests
+import re
+import time
+
+# Parse args from command line
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--download', action='store_true')
+parser.add_argument('-s', '--scan')
+args = parser.parse_args()
+
+# Using API available at https://www.xeno-canto.org/article/153
+request_url = "https://www.xeno-canto.org/api/2/recordings"
+pre_query = "?query="
+query = "cnt:United%States"
+
+# Download destination
+image_path = os.getcwd() + '/data/images/'
+
+start_time = time.time()
+
+r = requests.get(request_url + pre_query + query)
+
+if r.status_code == 200:
+    content = json.loads(r.text)
+else:
+    print("Error: status ", r.status_code)
+    quit()
+
+num_recordings = int(content['numRecordings'])
+num_species = int(content['numSpecies'])
+
+print(f"Payload with {num_recordings} recordings with {num_species} species after {time.time() - start_time} sec.")
+start_time = time.time()
+
+recordings = content['recordings']
+
+for i in range(100):
+    rec = recordings[i]
+
+    rec_url = rec['url']
+    rec_id = int(rec['id'])
+    rec_species = rec['sp']
+    req = requests.get(rec_url)
+    rec_text = req.text
+
+    print(f'Loaded id {rec_id} after {time.time() - start_time} sec. Species: {rec_species}')
+    start_time = time.time()
+
+    # download sonogram
+    if args.download:
+        # TODO: Scan rec_url, check if recording length is less than 10 sec
+
+        # Search the page corresponding to rec_id for a url ending in `large.png'. This is a sonogram (i.e. spectrograph)
+        sonogram_url = re.findall('www.*large.png', rec_text)[0]
+        f = open(image_path + str(rec_id) + '-' + rec_species + '.png', 'wb')
+        f.write(requests.get('http://' + sonogram_url).content)
+        f.close()
+
+        print(f'Downloaded {sonogram_url} after {time.time() - start_time} sec')
+        start_time = time.time()
+
+        #time.sleep(0.5)
